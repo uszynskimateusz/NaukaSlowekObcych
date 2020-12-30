7922de3a33d013c18da94de10180221ef5333e28
kubala918 <kubala.918@o2.pl>
2020-12-30 21:03:16 +0100

    podstawy

---
+++ NaukaSlowekObcych/Question.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaukaSlowekObcych
+{
+    class Question
+    {
+        Word word;
+        List<Word> answers = new List<Word>();
+        int questionid;
+        Level level;
+
+
+        void Request() { }
+    }
+}
---
+++ NaukaSlowekObcych/Word.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaukaSlowekObcych
+{
+    class Word
+    {
+        string polish;
+        string english;
+
+        public Word (string polish, string english)
+        {
+
+            polish = this.polish;
+            english = this.english;
+        }
+
+    }
+}
