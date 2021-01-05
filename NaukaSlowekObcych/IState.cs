31151ca679801e70c4f5b1bf9714bffaa2f05e91
PTK RTK <r.patryk98@gmail.com>
2021-01-05 17:34:13 +0100

    no cos tam jest

---
+++ NaukaSlowekObcych/IState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaukaSlowekObcych
+{
+    abstract class IState
+    {
+        protected Question question;
+        public void SetQuestion(Question question)
+        {
+            this.question = question;
+        }
+        public abstract void GetPoint();
+
+    }
+}
