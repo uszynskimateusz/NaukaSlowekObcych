8dbd18ea862af43f662249510a6090ecfb70280d
PTK RTK <r.patryk98@gmail.com>
2021-01-09 13:00:45 +0100

    state dodany

--- NaukaSlowekObcych/Question.cs
+++ NaukaSlowekObcych/Question.cs
@@ -15,5 +15,22 @@ namespace NaukaSlowekObcych
         //siema
 
         void Request() { }
+
+        //Od state
+        private IState state;
+
+        public Question(IState state)
+        {
+            this.ChangeState(state);
+        }
+
+
+        public void ChangeState(IState state)
+        {
+            this.state = state;
+            this.state.SetQuestion(this);
+        }
+        //Koniec od state
+
     }
 }
---
+++ NaukaSlowekObcych/StateTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaukaSlowekObcych
+{
+    class StateTest : IState
+    {
+        public override void GetPoint()
+        {
+            //tu jesli if prawdziwy np slowo nauczone to wtedy stan sie zmieni
+
+        }
+    }
+}
